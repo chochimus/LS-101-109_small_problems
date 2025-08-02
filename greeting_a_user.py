1fca4a3551b3841ca08e30bee5c3b8d22ab18a48
chochimus <127354521+chochimus@users.noreply.github.com>
2025-08-02 17:35:15 -0500

    Typo fix

--- greeting_a_user.py
+++ greeting_a_user.py
@@ -1,5 +1,5 @@
 name = input("What is your name? ")
 if name.endswith('!'):
-    print(f"HELLO {name.upper()}! WHY ARE WE YELLING?")
+    print(f"HELLO {name.upper()} WHY ARE WE YELLING?")
 else:
     print(f"Hello {name}.")
