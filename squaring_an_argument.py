af23af04c06389d06b533a37d9cc10164fd5027d
chochimus <127354521+chochimus@users.noreply.github.com>
2025-08-02 17:51:09 -0500

    Updated solution

--- squaring_an_argument.py
+++ squaring_an_argument.py
@@ -4,7 +4,15 @@ def multiply(num1, num2):
 def square(num):
     return multiply(num, num)
 
-# for n, take second param n and use as second argument to multiply.
+def nth_power(num, n):
+    value = 1
+    for _ in range(n):
+        value = multiply(num, value)
+    return value
+
+
+print(nth_power(2,2))
+print(nth_power(3,3))
 
 print(square(5) == 25)   # True
 print(square(-8) == 64)  # True
